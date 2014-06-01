90d4cd0e7edbbad0bc3b02d332507078fbf746e6
刘坤 <liukun927nh@163.com>
2014-06-01 15:06:18 +0800

    添加友盟统计sdk

--- KENDivination/Class/KENAppDelegate.m
+++ KENDivination/Class/KENAppDelegate.m
@@ -8,9 +8,24 @@
 
 #import "KENAppDelegate.h"
 #import "KENModel.h"
+#import "KENConfig.h"
 #import "KENCoreDataManager.h"
 
+#import "MobClick.h"
+
 @implementation KENAppDelegate
+
+-(void)umengTrack{
+//    [MobClick setCrashReportEnabled:NO]; // 如果不需要捕捉异常，注释掉此行
+//    [MobClick setLogEnabled:YES];  // 打开友盟sdk调试，注意Release发布时需要注释掉此行,减少io消耗
+    [MobClick setAppVersion:XcodeAppVersion]; //参数为NSString * 类型,自定义app版本信息，如果不设置，默认从CFBundleVersion里取
+
+    if (IsPad) {
+        [MobClick startWithAppkey:@"5388374756240b7bc700d1b3"];
+    } else {
+        [MobClick startWithAppkey:@"5388367d56240b7bd600bdbe"];
+    }
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions{
     //init
@@ -25,6 +40,9 @@
     
     [self.window addSubview:navigation.view];
     [self.window makeKeyAndVisible];
+    
+    //  友盟的方法本身是异步执行，所以不需要再异步调用
+    [self umengTrack];
 
     return YES;
 }
