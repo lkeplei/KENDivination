879eeaa32ab0cb5d79340f53486802a510a21e29
刘坤 <liukun927nh@163.com>
2014-11-12 20:42:48 +0800

    解析界面结构基本完成 还需要细节页实现

---
+++ KENDivination/Class/View/KENViewParePerson.h
@@ -0,0 +1,13 @@
+//
+//  KENViewParePerson.h
+//  KENDivination
+//
+//  Created by 刘坤 on 14-11-11.
+//  Copyright (c) 2014年 ken. All rights reserved.
+//
+
+#import "KENViewBase.h"
+
+@interface KENViewParePerson : KENViewBase
+
+@end
---
+++ KENDivination/Class/View/KENViewParePerson.m
@@ -0,0 +1,21 @@
+//
+//  KENViewParePerson.m
+//  KENDivination
+//
+//  Created by 刘坤 on 14-11-11.
+//  Copyright (c) 2014年 ken. All rights reserved.
+//
+
+#import "KENViewParePerson.h"
+
+@implementation KENViewParePerson
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
